d57c69836166f0946ff0fc4b182f0eb633d89422
Kevin <99073020@mydavinci.nl>
2023-03-09 23:14:40 +0100

    M2 | DOM Manipulatie & Event Handeling | Buttons 3.0

--- module_02-dom_manipulatie_&_event_handeling/buttons3/buttons.js
+++ module_02-dom_manipulatie_&_event_handeling/buttons3/buttons.js
@@ -1,6 +1,22 @@
-const buttonsPerRow = 5;
-const totalButtons = 30;
+const buttonsPerRow = 5; // Het aantal buttons per rij
+const totalButtons = 30; // Aantal buttons
 
-let rows = Math.ceil(buttons / buttonsPerRow);
+let rows = Math.ceil(totalButtons / buttonsPerRow); // Berekent hoeveel rijen er zijn
+let container = document.getElementById("container");
 
-let buttons = [];
+// Dit maakt alle buttons aan in de HTML pagina
+for (i = 0; i < rows; i++){
+    let row = document.createElement("div");
+    row.classList.add("button");
+
+    // Bepaal het aantal knoppen voor de huidige rij
+    let buttonsForRow = Math.min(buttonsPerRow, totalButtons - i * buttonsPerRow);
+
+    for (x = 0; x < buttonsForRow; x++){
+        let button = document.createElement("button");
+        button.innerHTML = `${i * buttonsPerRow + x + 1}`;
+        row.appendChild(button);
+    }
+
+    container.appendChild(row);
+}
